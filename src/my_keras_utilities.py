6d637aea199578d4f3575123a34e4961c4f1273f
Rubens Machado <rubens.campos.machado@gmail.com>
2017-06-21 12:33:04 -0300

    livros com sequencias de 200 palavras

--- src/my_keras_utilities.py
+++ src/my_keras_utilities.py
@@ -102,7 +102,7 @@ class TrainingPlotter(Callback):
                     self.line2 = self.axis.plot(htrain, linewidth=2, label='training mse')[0]
                     if val:
                         self.line1 = self.axis.plot(hvalid, linewidth=2, label='validation mse')[0]
-                        self.axis.vlines(self.best_epoch, 0, 1, colors='#EBDDE2', linestyles='dashed', 
+                        self.axis.vlines(self.best_epoch, 0, 100, colors='#EBDDE2', linestyles='dashed', 
                                          label='validation min')
                 else:
                     self.line2.set_xdata(np.arange(htrain.shape[0]))
@@ -110,7 +110,7 @@ class TrainingPlotter(Callback):
                     if val:
                         self.line1.set_xdata(np.arange(hvalid.shape[0]))
                         self.line1.set_ydata(hvalid)
-                        self.axis.vlines(self.best_epoch, 0, 1, colors='#EBDDE2', linestyles='dashed')
+                        self.axis.vlines(self.best_epoch, 0, 100, colors='#EBDDE2', linestyles='dashed')
                     
                 self.axis.legend()
                 if 'val_acc' in logs.keys():
